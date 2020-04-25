38a5b5fc43d04a28125eb1ce59ad0b599c772f3a
cohix <connorjhicks@gmail.com>
2020-04-25 11:44:47 -0400

    removed rs-builder from this repo

--- rs-builder/build.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-set -e
-
-cp ../rs-wasm/* ./src/
-wasm-pack build
-
-cp pkg/wasm_runner_bg.wasm ../rs-wasm
--- rs-builder/src/run.rs
+++
@@ -1,8 +0,0 @@
-// this file only exists to allow the Docker image to pre-build the cargo dependencies and will be replaced by the user's run.rs
-
-pub fn run(input: String) -> Option<String> {
-    
-    let out = String::from(format!("hello {}", input));
-    
-    return Some(out);
-}
