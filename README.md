86ba7e65eb344c82e3861ada118608ff849713df
cohix <connorjhicks@gmail.com>
2020-09-15 08:57:58 -0400

    add data type conversion and update README

--- README.md
+++ README.md
@@ -4,7 +4,17 @@ WASM toolchain for [Hive](https://github.com/suborbital/hive)
 
 The Hive job scheduler aims for WASM to be a first-class citizen. `hivew` is the toolchain for Hive and WASM. The `hivew` CLI can build WASM Runnables, and can package many WASM Runnables into a deployable bundle. It will soon be able to act as an all-in-one Hive server, using Hive's FaaS functionality.
 
-Docker must be installed to build WASM Runnables.
+Writing a Runnable for Hive in languages other than Go is designed to be just as simple and powerful:
+```rust
+#[no_mangle]
+pub fn run(input: Vec<u8>) -> Option<Vec<u8>> {
+    let in_string = String::from_utf8(input).unwrap();
+
+    Some(String::from(format!("hello {}", in_string)).as_bytes().to_vec())
+}
+```
+hivew will package your Runnable into a WASM module that can be loaded into a Hive instance and run just like any other Runnable!
+
 
 ## Installing
 To install `hivew`, clone this repo and run `make hivew`. A version of Go that supports Modules is required. Package manager installations will be available soon.
@@ -14,7 +24,10 @@ You can also install using [gobinaries](https://gobinaries.com/):
 curl -sf https://gobinaries.com/suborbital/hivew/hivew | sh
 ```
 
-## Building Runnables
+## Building WASM Runnables
+**Docker must be installed to build WASM Runnables.**
+The hivew CLI builds your Runnable code into a WASM module that can be loaded by Hive.
+
 To build a Rust-based Runnable, see [helloworld-rs](./helloworld-rs/README.md)
 
 ## Bundles
@@ -23,3 +36,9 @@ To build all of the Runnables in the current directory and bundle them all into 
 `hivew` is under active development alongside [Hive](https://github.com/suborbital/hive) itself.
 
 Copyright Suborbital contributors 2020
+
+## FFI Runnable API
+hivew provides an API which allows for communication between WASM runnables and Hive. This API is currently limited to internal functions used to run jobs and return their results, but in the future this API will expand to include:
+- The ability to make network requests from WASM Runnables (with built-in access controls to restrict network activity)
+- The ability to read files from the host machine (with build-in access control)
+- The ability to schedule new Hive jobs and get their results (similar to the Go Runnable API)
--- wasm/environment.go
+++ wasm/environment.go
@@ -13,27 +13,22 @@ import (
 // extern void return_result(void *context, int32_t pointer, int32_t size, int32_t envIndex, int32_t instIndex);
 import "C"
 
-//export return_result
-func return_result(context unsafe.Pointer, pointer int32, size int32, envIndex int32, instIndex int32) {
-	envLock.RLock()
-	defer envLock.RUnlock()
+/*
+ In order to allow "easy" communication of data across the FFI barrier (outbound Go -> WASM and inbound WASM -> Go), hivew provides
+ an FFI API. Functions exported from a WASM module can be easily called by Go code via the Wasmer instance exports, but returning data
+ to the host Go code is not quite as straightforward.
 
-	env := environments[envIndex]
-	inst := env.instances[instIndex]
+ In order to accomplish this, hivew internally keeps a set of "environments" in a singleton package var (`environments` below).
+ Each environment is a container that includes the WASM module bytes, and a set of WASM instances (runtimes) to execute said module.
+ The envionment object has an index referencing its place in the singleton array, and each instance has an index referencing its position within
+ the environment's instance array.
 
-	memory := inst.wasmerInst.Memory.Data()[pointer:]
-
-	result := make([]byte, size)
-
-	for index := 0; int32(index) < size; index++ {
-		result[index] = memory[index]
-	}
-
-	inst.resultChan <- result
-}
+ When a WASM function calls one of the FFI API functions, it includes the `env_index` and `inst_index` values that were provided at the beginning
+ of job execution, which allows hivew to look up the [env][instance] and send the result on the appropriate result channel. This is needed due to
+ the way Go makes functions available on the FFI using CGO.
+*/
 
 var environments []*wasmEnvironment
-var envOnce = sync.Once{}
 var envLock = sync.RWMutex{}
 
 // wasmEnvironment is an wasmEnvironment in which WASM instances run
@@ -53,7 +48,7 @@ type wasmInstance struct {
 	resultChan chan []byte
 	lock       sync.Mutex
 
-	// meta related to this env's position in the shared array, and the index of the last used wasmInstance
+	// meta related to this env's position in the shared array, and the index of this wasmInstance in the environment
 	envIndex  int
 	instIndex int
 }
@@ -61,10 +56,6 @@ type wasmInstance struct {
 // newEnvironment creates a new environment and adds it to the shared environments array
 // such that WASM instances can return data to the correct place
 func newEnvironment(name string, filepath string) *wasmEnvironment {
-	envOnce.Do(func() {
-		environments = []*wasmEnvironment{}
-	})
-
 	envLock.Lock()
 	defer envLock.Unlock()
 
@@ -159,4 +150,32 @@ func (w *wasmInstance) deallocate(pointer int32, length int) {
 	dealloc := w.wasmerInst.Exports["deallocate"]
 
 	dealloc(pointer, length)
+}
+
+///////////////////////////////////////////////////////////////
+// below is the "hivew API" used for cross-FFI communication //
+///////////////////////////////////////////////////////////////
+
+//export return_result
+func return_result(context unsafe.Pointer, pointer int32, size int32, envIndex int32, instIndex int32) {
+	envLock.RLock()
+	defer envLock.RUnlock()
+
+	env := environments[envIndex]
+	inst := env.instances[instIndex]
+
+	memory := inst.wasmerInst.Memory.Data()[pointer:]
+
+	result := make([]byte, size)
+
+	for index := 0; int32(index) < size; index++ {
+		result[index] = memory[index]
+	}
+
+	inst.resultChan <- result
+}
+
+func init() {
+	environments = []*wasmEnvironment{}
+	envLock = sync.RWMutex{}
 }
